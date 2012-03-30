4c3993ce64134bb322215caab4c458e5aedc32e3
Bryan Larsen <bryan@larsen.st>
2012-03-29 21:01:12 -0400

    fix "edit this file" links

--- lib/hobo_simple_color.rb
+++ lib/hobo_simple_color.rb
@@ -3,7 +3,7 @@ module HoboSimpleColor
   @@root = Pathname.new File.expand_path('../..', __FILE__)
   def self.root; @@root; end
 
-  EDIT_LINK_BASE = 'https://github.com/my_github_username/hobo_simple_color/edit/master'
+  EDIT_LINK_BASE = 'https://github.com/Hobo/hobo_simple_color/edit/master'
 
   if defined?(Rails)
     require 'hobo_simple_color/railtie'
